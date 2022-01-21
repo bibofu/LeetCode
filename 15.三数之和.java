b652cf96d85f4510312021d3cbacde1ccc65d498
bibofu <bibofu24@gmail.com>
2022-01-21 23:01:18 +0800

    lc15

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -7,40 +7,56 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
+        
         List<List<Integer>> res=new ArrayList<>();
+        if(nums.length<3){
+            return res;
+        }
+        
         Arrays.sort(nums);
-
-        for(int i=0;i<nums.length;i++){
+        
+        //大胆尝试
+        for(int i=0;i<nums.length-1;i++){
             if(i>=1&&nums[i]==nums[i-1]){
                 continue;
             }
+            
+            //双指针
             int left=i+1;
             int right=nums.length-1;
             while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
+                int temp=nums[i]+nums[left]+nums[right];
+                if(temp<0){
+                    left++;
+                }else if(temp>0){
+                    right--;
+                }else{
                     while(left<right&&nums[left]==nums[left+1]){
                         left++;
                     }
-
+                    
                     while(left<right&&nums[right]==nums[right-1]){
                         right--;
                     }
-
-                    List<Integer> temp=Arrays.asList(nums[i],nums[left],nums[right]);
-                    res.add(new ArrayList<>(temp));
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    
                     left++;
                     right--;
-                }else if(sum<0){
-                    left++;
-                }else{
-                    right--;
+                    
+                    res.add(list);
+                    
                 }
             }
+            
+            
         }
-
+        
         return res;
+        
+        
 
     }
 }
