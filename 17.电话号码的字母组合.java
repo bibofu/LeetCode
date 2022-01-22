ff7412ad08dc38cc714db9ecfb7d9a8f3f94f0e6
bibofu <bibofu24@gmail.com>
2022-01-22 14:26:44 +0800

    lc18

--- 17.电话号码的字母组合.java
+++ 17.电话号码的字母组合.java
@@ -6,8 +6,59 @@
 
 // @lc code=start
 class Solution {
+    List<String> res=new ArrayList<>();
     public List<String> letterCombinations(String digits) {
+        if(digits.length()==0){
+            return res;
+        }
+        
+        helper(digits,"");
+        return res;
 
+    }
+    
+    public void helper(String s,String temp){
+        if(s.length()==0){
+            res.add(temp);
+            temp=temp.substring(0,temp.length()-1);
+            return ;
+            
+        }
+        if(s.charAt(0)=='2'){
+            helper(s.substring(1),temp+"a");
+            helper(s.substring(1),temp+"b");
+            helper(s.substring(1),temp+"c");
+        }else if(s.charAt(0)=='3'){
+            helper(s.substring(1),temp+"d");
+            helper(s.substring(1),temp+"e");
+            helper(s.substring(1),temp+"f");
+        }else if(s.charAt(0)=='4'){
+            helper(s.substring(1),temp+"g");
+            helper(s.substring(1),temp+"h");
+            helper(s.substring(1),temp+"i");
+        }else if(s.charAt(0)=='5'){
+            helper(s.substring(1),temp+"j");
+            helper(s.substring(1),temp+"k");
+            helper(s.substring(1),temp+"l");
+        }else if(s.charAt(0)=='6'){
+            helper(s.substring(1),temp+"m");
+            helper(s.substring(1),temp+"n");
+            helper(s.substring(1),temp+"o");
+        }else if(s.charAt(0)=='7'){
+            helper(s.substring(1),temp+"p");
+            helper(s.substring(1),temp+"q");
+            helper(s.substring(1),temp+"r");
+            helper(s.substring(1),temp+"s");
+        }else if(s.charAt(0)=='8'){
+            helper(s.substring(1),temp+"t");
+            helper(s.substring(1),temp+"u");
+            helper(s.substring(1),temp+"v");
+        }else if(s.charAt(0)=='9'){
+            helper(s.substring(1),temp+"w");
+            helper(s.substring(1),temp+"x");
+            helper(s.substring(1),temp+"y");
+            helper(s.substring(1),temp+"z");
+        }
     }
 }
 // @lc code=end
--- 18.四数之和.java
+++ 18.四数之和.java
@@ -7,7 +7,56 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
+        
+        List<List<Integer>> res=new ArrayList<>();
+        if(nums.length<4){
+            return res;
+            
+        }
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length;i++){
+            for(int j=nums.length-1;j>=3;j--){
+                if(i>=1&&nums[i]==nums[i-1]&&i<=j-3){
+                    break;
+                }
+                int left=i+1;
+                int right=j-1;
+                if(left>=right){
+                    break;
+                }
+                
+                while(left<right){
+                    if(left>=right){
+                        break;
+                    }
+                    int temp=nums[i]+nums[j]+nums[left]+nums[right];
+                    if(temp==target){
+                        while(left<right-1&&nums[left+1]==nums[left]){
+                            left++;
+                        }
+                        while(left<right-1&&nums[right]==nums[right-1]){
+                            right--;
+                        }
+                        List<Integer> list=Arrays.asList(nums[i],nums[left],nums[right],nums[j]);
+                        if(!res.contains(list)){
+                            res.add(list);
+                        }
+                   
+                        left++;
+                        right--;
+                    }else if(temp<target){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+                
+            }
+        }
+        
+        return res;
     }
 }
 // @lc code=end
