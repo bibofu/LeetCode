b347780e052f8b451d18dedeeb12eae75a92b31e
bibofu <bibofu24@gmail.com>
2022-02-24 23:36:30 +0800

    2 24

--- 160.相交链表.java
+++ 160.相交链表.java
@@ -18,25 +18,26 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
         ListNode cur1=headA;
         ListNode cur2=headB;
-
         while(cur1!=cur2){
             if(cur1!=null){
                 cur1=cur1.next;
-
             }else{
                 cur1=headB;
             }
-
+            
             if(cur2!=null){
                 cur2=cur2.next;
             }else{
                 cur2=headA;
             }
         }
-
+        
         return cur1;
+        
+        
     }
 }
 // @lc code=end
