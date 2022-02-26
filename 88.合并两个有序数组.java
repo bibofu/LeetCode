1603e1d706b32aea4f847f39c6be12cf0a3d23b9
bibofu <bibofu24@gmail.com>
2022-02-26 17:11:52 +0800

    2 26 add

--- 88.合并两个有序数组.java
+++ 88.合并两个有序数组.java
@@ -6,29 +6,31 @@
 
 // @lc code=start
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
         int index1=m-1;
         int index2=n-1;
-        int index=nums1.length-1;
-
-        while(index1>=0||index2>=0){
-            if(index1<0){
-                nums1[index]=nums2[index2];
-                index2--;
-            }else if(index2<0){
-                break;
-            }else if(nums1[index1]>nums2[index2]){
+        int index=m+n-1;
+        
+        while(index1>=0&&index2>=0){
+            if(nums1[index1]>=nums2[index2]){
                 nums1[index]=nums1[index1];
                 index1--;
+                index--;
             }else{
                 nums1[index]=nums2[index2];
                 index2--;
+                index--;
             }
-
-            index--;
-
         }
+        
+        while(index2>=0){
+            nums1[index]=nums2[index2];
+            index2--;
+            index--;
+        }
+
 
     }
 }
