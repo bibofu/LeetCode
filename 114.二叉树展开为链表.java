14ba47b965fdfa41be414915eedf212f0e3b162d
bibofu <bibofu24@gmail.com>
2022-03-01 14:42:07 +0800

    3 1

--- 114.二叉树展开为链表.java
+++ 114.二叉树展开为链表.java
@@ -22,7 +22,27 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
+        if(root==null){
+            return ;
+        }
 
+        Stack<TreeNode> stack=new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            TreeNode node=stack.pop();
+            if(node.right!=null){
+                stack.push(node.right);
+            }
+            if(node.left!=null){
+                stack.push(node.left);
+            }
+            if(!stack.isEmpty()){
+                node.right=stack.peek();
+            }
+            node.left=null;
+
+        }
     }
 }
 // @lc code=end
