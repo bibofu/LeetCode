8eb659cf666dc88213ae7890509e60e4311f0d78
bibofu <bibofu24@gmail.com>
2022-03-03 23:58:19 +0800

    3 3 night

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -7,27 +7,20 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],i);
         }
 
-        int[] res=new int[2];
-
         for(int i=0;i<nums.length;i++){
-            int need=target-nums[i];
-            if(map.containsKey(need)&&map.get(need)!=i){
-                res[0]=i;
-                res[1]=map.get(need);
-                break;
-
-
+            int left=target-nums[i];
+            if(map.containsKey(left)&&map.get(left)!=i){
+                return new int[]{i,map.get(left)};
             }
         }
 
-        return res;
-
-        
+        return null;
 
     }
 }
