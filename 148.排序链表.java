29171573c48f0f8a477258404033581ba092e05f
bibofu <bibofu24@gmail.com>
2022-03-04 16:28:26 +0800

    3 4

--- 148.排序链表.java
+++ 148.排序链表.java
@@ -18,68 +18,59 @@
 class Solution {
     public ListNode sortList(ListNode head) {
 
-         // 1、递归结束条件
-        if (head == null || head.next == null) {
+        if(head==null||head.next==null){
             return head;
         }
 
-        // 2、找到链表中间节点并断开链表 & 递归下探
-        ListNode midNode = getmid(head);
-        ListNode rightHead = midNode.next;
-        midNode.next = null;
+        ListNode mid=getMid(head);
+        ListNode right=mid.next;
+        mid.next=null;
+        ListNode a=sortList(head);
+        ListNode b=sortList(right);
 
-        ListNode left = sortList(head);
-        ListNode right = sortList(rightHead);
-
-        // 3、当前层业务操作（合并有序链表）
-        return merge(left, right);
-
+        return merge(a,b);
     }
 
-    public ListNode getmid(ListNode head){
+    public ListNode getMid(ListNode head){
         if(head==null||head.next==null){
             return head;
         }
 
         ListNode slow=head;
         ListNode fast=head.next;
-
         while(fast!=null&&fast.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
+            slow=slow.next;
         }
-
         return slow;
-        
     }
 
-    public ListNode merge(ListNode l1,ListNode l2){
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
+    public ListNode merge(ListNode node1,ListNode node2){
 
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
+        ListNode dummy=new ListNode(-1);
+        ListNode cur=dummy;
+        while(node1!=null&&node2!=null){
+            if(node1.val<=node2.val){
+                cur.next=node1;
+                node1=node1.next;
+            }else{
+                cur.next=node2;
+                node2=node2.next;
             }
-
-            cur = cur.next;
+            cur=cur.next;
         }
 
-        if(l1!=null){
-            cur.next=l1;
+        if(node1!=null){
+            cur.next=node1;
         }
-        if(l2!=null){
-            cur.next=l2;
+        if(node2!=null){
+            cur.next=node2;
         }
-
         
         return dummy.next;
-
     }
+
+   
 }
 // @lc code=end
 
