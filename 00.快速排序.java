04aec6a195b7e1050bb40286430a27d3ed7784b8
bibofu <bibofu24@gmail.com>
2022-03-04 15:59:06 +0800

    quicksort & mergesort

---
+++ 00.快速排序.java
@@ -0,0 +1,107 @@
+// 快速排序的两种实现
+
+
+//递归
+
+class Solution{
+
+    public int[] quick(int[] arr,int left,int right){
+        if(left<right){
+            int find=partition(arr,left,right);
+            quick(arr,left,find-1);
+            quick(arr,find+1,right)
+        }
+        return arr;
+    }
+    //example
+    //arr={4,5,3,1,7} left=0,right=4
+    //arr= 4 1 3 5 7
+    //arr= 3 1 4 5 7
+    //把小于pivot的值放到左边，大于的放到右边，返回pivot对应的索引
+    public int partition(int[] arr,int left,int right){
+        int pivot=arr[left];
+        int start=left;
+        while(left<right){
+            while(left<right&&arr[right]>=pivot){
+                right--;
+            }
+
+            while(left<right&&arr[left]<=pivot){
+                left++;
+            }
+
+            if(left>=right){
+                break;
+            }
+            swap(arr,left,right);
+        }
+
+        swap(arr,start,left);
+        return left;
+
+    }
+
+    public void swap(int[] arr,int left,int right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+    }
+}
+
+//非递归
+
+class Solution{
+
+    public int[] quick(int[] arr){
+        Stack<Integer> stack=new Stack<>();
+        stack.push(arr.length-1);
+        stack.push(0);
+        while(!stack.isEmpty()){
+            int lo=stack.pop();
+            int hi=stack.pop();
+            if(lo<hi){
+                int index=partition(arr,lo,hi);
+                stack.push(index-1);
+                stack.push(lo);
+                stack.push(index+1);
+                stack.push(hi);
+
+            }
+        }
+
+        return arr;
+    }
+
+
+
+
+    public int partition(int[] arr,int left,int right){
+        int pivot=arr[left];
+        int start=left;
+        while(left<right){
+            while(left<right&&arr[right]>=pivot){
+                right--;
+            }
+            while(left<right&&arr[left]<=pivot){
+                left++;
+            }
+            if(left>=right){
+                break;
+            }
+            swap(arr,left,right);
+        }
+        swap(arr,start,left);
+        return left;
+    }
+
+
+    public void swap(int[] arr,int left,int right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+    }
+
+
+
+
+}
---
+++ 01.归并排序.java
@@ -0,0 +1,84 @@
+//归并排序的实现
+
+class Solution{
+
+
+
+    public int[] merge(int[] arr){
+        int left=0;
+        int right=arr.length-1;
+        int[] temp=new int[arr.length];
+        sort(arr,left,right,temp);
+        return arr;
+    }
+
+    public void sort(int[] arr,int left,int right,int[] temp){
+        if(left<right){
+            int mid=left+(right-left)/2;
+            sort(arr,left,mid,temp);
+            sort(arr,mid+1,right,temp);
+            merge(arr,left,right,temp);
+        }
+
+    }
+
+
+    //借助数组temp完成排序
+    public void merge(int[] arr,int left,int right,int[] temp){
+        int mid=left+(right-left)/2;
+        int i=left;
+        int j=mid+1;
+        int t=0;
+        
+        while(i<=mid&&j<=right){
+            if (arr[i]<arr[j]){
+                temp[t]=arr[i];
+                i++;
+                t++;
+            }else {
+                temp[t]=arr[j];
+                j++;
+                t++;
+            }
+
+        }
+
+        while (i<=mid){
+            temp[t]=arr[i];
+            i++;
+            t++;
+        }
+
+        while (j<=right){
+            temp[t]=arr[j];
+            j++;
+            t++;
+        }
+
+        t=0;
+        while(left<=right){
+            arr[left]=temp[t];
+            left++;
+            t++;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
