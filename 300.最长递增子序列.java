58917393d3cdda60adb4cde248bc0c6fd4ed1a30
bibofu <bibofu24@gmail.com>
2022-03-08 14:43:18 +0800

    3 8

--- 300.最长递增子序列.java
+++ 300.最长递增子序列.java
@@ -8,24 +8,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
 
-        int[] dp=new int[nums.length];
-        int max=1;
-        dp[0]=1;
-
-        for(int i=1;i<nums.length;i++){
-            dp[i]=1;
-
+        int n=nums.length;
+        int[] dp=new int[n];
+        int max=0;
+        for(int i=0;i<n;i++){
+            int temp=1;
             for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]&&dp[j]+1>dp[i]){
-                    dp[i]=dp[j]+1;
+                if(nums[j]<nums[i]){
+                    temp=Math.max(temp,dp[j]+1);
                 }
-
-
             }
+            dp[i]=temp;
             if(dp[i]>max){
                 max=dp[i];
             }
-
         }
 
         return max;
