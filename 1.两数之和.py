cc3faa02d32631a03c02ae72d24890f19d54e90e
bibofu <bibofu24@gmail.com>
2022-04-06 23:17:09 +0800

    4 6

---
+++ 1.两数之和.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map={} #val index
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in map:
+                return [map[diff],i]
+            map[n]=i
+        return 
+# @lc code=end
+
